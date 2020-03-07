9d58c907a3b65204836ff2ac2e7f251a2440f5f5
voidful <voidful.stack@gmail.com>
2020-03-07 22:54:10 +0800

    update readme
    Former-commit-id: 1a0369067c182a4b958daabd9afbb27a5d661c56

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfkit',
-    version='0.1.18',
+    version='0.1.19',
     description='Transformers kit - NLP library for different downstream tasks, built on huggingface project ',
     url='https://github.com/voidful/TFkit',
     author='Voidful',
@@ -20,7 +20,7 @@ setup(
     keywords='transformer huggingface nlp multi-task multi-class multi-label classification generation tagging deep learning machine reading',
     packages=find_packages(),
     install_requires=[
-        "transformers==2.5.0",
+        "transformers==2.5.1",
         "tensorboardX",
         "torch",
         "sklearn",
