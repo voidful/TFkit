bbe123d2a195ab5f050b044a6e1b06845387eea0
voidful <voidful.stack@gmail.com>
2020-04-10 05:51:41 +0800

    add pretrain dump & update benchmark

---
+++ tfkit/test/test_dump.py
@@ -0,0 +1,15 @@
+import unittest
+
+import os
+
+
+class TestEval(unittest.TestCase):
+
+    def testHelp(self):
+        result = os.system('tfkit-dump -h')
+        assert (result == 0)
+
+    def testDump(self):
+        result = os.system(
+            'tfkit-dump --model ./cache/10.pt --dumpdir ./cache/dump')
+        print(result)
