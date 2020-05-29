c446a9fb35763f576eebc681ff6d33ce240045ac
voidful <voidful.stack@gmail.com>
2020-05-29 23:36:25 +0800

    fix decode bug on gpt model

--- tfkit/utility/tok.py
+++ tfkit/utility/tok.py
@@ -4,3 +4,7 @@ def tok_begin(tokenizer):
 
 def tok_sep(tokenizer):
     return tokenizer._sep_token or tokenizer._eos_token or 'sep'
+
+
+def tok_mask(tokenizer):
+    return tokenizer._mask_token or 'msk'
