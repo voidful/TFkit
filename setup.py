eef0965700bece51009b81ea16bbc8e1b6a0aab4
voidful <voidful.stack@gmail.com>
2020-07-06 05:16:29 +0800

    update requirement to fix panel input function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfkit',
-    version='0.3.45',
+    version='0.3.46',
     description='Transformers kit - NLP library for different downstream tasks, built on huggingface project ',
     url='https://github.com/voidful/TFkit',
     author='Voidful',
@@ -25,7 +25,7 @@ setup(
         "torch",
         "sklearn",
         "matplotlib",
-        "nlp2>=1.7.0",
+        "nlp2>=1.7.1",
         "tqdm>=4.45.0",
         "inquirer"
     ],
