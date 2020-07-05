ad27fe57b638ca9912d52ec4868573601fb80706
voidful <voidful.stack@gmail.com>
2020-07-06 04:51:02 +0800

    update requirement to remove unless panel input
    Former-commit-id: fc45dd48bd79df2ad75dac1163e8f52a41ac4cf1

--- tfkit/eval.py
+++ tfkit/eval.py
@@ -86,37 +86,10 @@ def load_model(model_path, model_type=None, model_dataset=None):
         return model
 
 
-def get_class_that_defined_method(meth):
-    if inspect.ismethod(meth):
-        for cls in inspect.getmro(meth.__self__.__class__):
-            if cls.__dict__.get(meth.__name__) is meth:
-                return cls
-        meth = meth.__func__  # fallback to __qualname__ parsing
-    if inspect.isfunction(meth):
-        cls = getattr(inspect.getmodule(meth),
-                      meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
-        if isinstance(cls, type):
-            return cls
-    return getattr(meth, '__objclass__', None)  # handle special descriptor objects
-
-
-def get_class_that_defined_method(meth):
-    if inspect.ismethod(meth):
-        for cls in inspect.getmro(meth.__self__.__class__):
-            if cls.__dict__.get(meth.__name__) is meth:
-                return cls
-        meth = meth.__func__  # fallback to __qualname__ parsing
-    if inspect.isfunction(meth):
-        cls = getattr(inspect.getmodule(meth),
-                      meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
-        if isinstance(cls, type):
-            return cls
-    return None  # not required since None would have been implicitly returned anyway
-
-
 def load_predict_parameter(model, enable_arg_panel=False):
     """use inquirer panel to let user input model parameter or just use default value"""
-    return nlp2.function_argument_panel(model.predict, disable_input_panel=(not enable_arg_panel), func_parent=model)
+    return nlp2.function_argument_panel(model.predict, disable_input_panel=(not enable_arg_panel), func_parent=model,
+                                        ignore_empty=True)
 
 
 def main():
