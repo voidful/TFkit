e9891e19200f74c7f891552880b694ba810751d6
voidful <voidful.stack@gmail.com>
2020-07-25 20:57:39 +0800

    remove file update setup and ignore
    Former-commit-id: bd0c787a1399bf3b7265898c756df85c2478a11b

--- cache/gen_twice/__init__.py
+++
@@ -1,1 +0,0 @@
-from .twice import *
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='voidful.stack@gmail.com',
     long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
+    setup_requires=['setuptools-git'],
     classifiers=[
         'Development Status :: 4 - Beta',
         "Intended Audience :: Science/Research",
