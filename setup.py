fde1fd0119dcb34786384c08749a018c22d183bb
voidful <voidful.stack@gmail.com>
2020-09-20 16:54:11 +0800

    add pos and neg ratio to cache info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfkit',
-    version='0.3.88',
+    version='0.3.89',
     description='Transformers kit - NLP library for different downstream tasks, built on huggingface project ',
     url='https://github.com/voidful/TFkit',
     author='Voidful',
