c85e52388cf0dd1a0e86649977eab3a05109ce67
voidful <voidful.stack@gmail.com>
2020-10-17 18:56:50 +0800

    fix(TEST-QA): topK
    element position error

--- tfkit/test/test_model.py
+++ tfkit/test/test_model.py
@@ -77,8 +77,8 @@ class TestModel(unittest.TestCase):
         # test eval top k = 2
         top_k_label, top_k_dict = model.predict(input=input, topk=2)
         print(top_k_label)
-        print("test predict with top k 2, ", top_k_label[0][0])
-        print("test predict with top k 2, ", top_k_label[0][1])
+        print("test predict with top k 2, ", top_k_label[0])
+        print("test predict with top k 2, ", top_k_label[1])
         self.assertTrue(len(top_k_label) == 2)
 
         # test exceed 512
