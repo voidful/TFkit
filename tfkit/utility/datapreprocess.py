3e6c7c43cfdd80fffe0d368c8ff24a7df7b05198
voidful <voidful.stack@gmail.com>
2022-01-11 10:55:53 +0800

    handle only one item

--- tfkit/utility/datapreprocess.py
+++ tfkit/utility/datapreprocess.py
@@ -13,17 +13,14 @@ class GeneralNLPPreprocessor:
     def custom_preprocess_fn(self, item, **param_dict):
         yield item
 
-    def prepare(self, items):
-        items = [items] if not isinstance(items, list) else items
-        print("Preprocessing File...")
-        for item in tqdm(items):
-            maxlen = self.parameters.get('maxlen')
-            t_input_list, _ = tok.handle_exceed(self.tokenizer, item['input'],
-                                                maxlen=maxlen - 3,
-                                                mode=self.parameters.get('handle_exceed'))
-            for t_input in t_input_list:
-                slice_length = maxlen - self.parameters.get('reserved_len') - 3
-                item['input'] = [tok.tok_begin(self.tokenizer)] + t_input[:slice_length]
-                for covert_feature_input_dict in self.custom_preprocess_fn(item, **self.parameters):
-                    self.preprocessed_data.append(covert_feature_input_dict)
+    def prepare(self, item):
+        maxlen = self.parameters.get('maxlen')
+        t_input_list, _ = tok.handle_exceed(self.tokenizer, item['input'],
+                                            maxlen=maxlen - 3,
+                                            mode=self.parameters.get('handle_exceed'))
+        for t_input in t_input_list:
+            slice_length = maxlen - self.parameters.get('reserved_len') - 3
+            item['input'] = [tok.tok_begin(self.tokenizer)] + t_input[:slice_length]
+            for covert_feature_input_dict in self.custom_preprocess_fn(item, **self.parameters):
+                self.preprocessed_data.append(covert_feature_input_dict)
         return self.preprocessed_data
